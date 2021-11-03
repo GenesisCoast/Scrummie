05d8dbce1cf2f3630e7a9915edcc3c3fb66d29f5
Harry Sanderson <h.sanderson@avanade.com>
2021-11-03 17:44:30 +0000

    Latest commit

--- api/src/Scrummie.API/Startup.cs
+++ api/src/Scrummie.API/Startup.cs
@@ -38,27 +38,6 @@ namespace Scrummie.API
         /// </summary>
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", " v1"));
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
-        }
 
         /// <summary>
         /// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
